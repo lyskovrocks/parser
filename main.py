cf386dff618c0f30d2b2ba6ee32c2488f91b51e6
lyskovrocks <lyskov.alex@gmail.com>
2023-08-23 18:09:44 +0300

    Добавлена функция для магазина мир музыки

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ def get_price_from_popmusic(product):
     web_site_text = requests.get(web_site + find_product).text
     soup = BeautifulSoup(web_site_text, 'html.parser')
     price_today = soup.find(class_='productfull__newprice').text
-    return price_today
+    telegram_sender.send_message(price_today)
 
-telegram_sender.send_message(get_price_from_popmusic('gitarnaya-radiosistema-line-6-relay-g30-888880006013/'))
+get_price_from_popmusic('gitarnaya-radiosistema-line-6-relay-g30-888880006013/')
+
+
+def get_price_from_mirm(product):
+
+    web_site = 'https://mirm.ru/catalog/products/'
+    find_product = product
+
+    web_site_text = requests.get(web_site + find_product).text
+    soup = BeautifulSoup(web_site_text, 'html.parser')
+    price_today = soup.find(id="results-price").text
+
+    print(price_today)
+    telegram_sender.send_message(price_today)
+
+get_price_from_mirm('line_6_relay_g30')
