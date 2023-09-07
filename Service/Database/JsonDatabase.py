0d6db5f39c1bf0c5ef357ee207956f71e639d148
lyskovrocks <lyskov.alex@gmail.com>
2023-09-07 19:14:39 +0300

    New Structure

---
+++ Service/Database/JsonDatabase.py
@@ -0,0 +1,2 @@
+class JsonDataBase:
+    pass
--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-web_site = "https://pop-music.ru/products/gitarnaya-radiosistema-line-6-relay-g30-888880006013/"
-web_site_text = requests.get(web_site).text
-soup = BeautifulSoup(web_site_text, 'html.parser')
-print(soup.find_all(class_ = 'productfull__newprice')[0].text)
-
-
